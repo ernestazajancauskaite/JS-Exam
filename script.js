48b62f7c34f55ce20bc6885ab71eb23d2c56cac3
ernestazajancauskaite <101720004+ernestazajancauskaite@users.noreply.github.com>
2022-06-01 21:08:34 +0300

    Task2
    +1 click, atlikta, viskas veikia

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-/* ------------------------------ TASK 1 ----------------------------
-Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
-pamatyti jo pateikto svorio kovertavimą į:
-1. Svarus (lb) | Formulė: lb = kg * 2.2046
-2. Gramus (g) | Formulė: g = kg / 0.0010000
-3. Uncijos (oz) | Formul4: oz = kg * 35.274
-
-Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
-<div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
-document.getElementById("search").addEventListener("input", function(e) {
-  let kg = e.target.value;
-  document.getElementById("LbsOutput").innerHTML = kg * 2.2046 ;
-  document.getElementById("GOutput").innerHTML = kg / 0.0010000 ;
-  document.getElementById("OzOutput").innerHTML = kg * 35.274 ;
+
+
+let counter = 0;
+
+document.getElementById("btn__element"),addEventListener(`click`,() => {
+    counter++
+    document.getElementById("btn__state").innerText = counter;
 });
