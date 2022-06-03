a31a12114df82b0ae7ec12b4bad2deddbb92896f
ernestazajancauskaite <101720004+ernestazajancauskaite@users.noreply.github.com>
2022-06-03 10:37:19 +0300

    Task 7

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
-/* ------------------------------ TASK 8 --------------------------------------------
-Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri gebės sukurti objektus su 4 metodais:
-sum(a, b) - priima du skaičius ir grąžina jų sumą.
-subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
-multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
-division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
------------------------------------------------------------------------------------- */
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
 
+Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
 
+console.log(audi)
 
-// Konstruktoriaus funkcija "Calculator"
-function Calculator() {
-    return {
-      sum: (a, b) => { 
-          return a + b; 
-        },
-      subtraction: (a, b) => {
-           return a - b; 
-        },
-      division: (a, b) => { 
-          return a / b; 
-        },
-      multiplication: (a, b) => { 
-          return a * b; 
-        }
-    };
-  }
-  
-  // keturi objektai sukurti pasinaudojant ta pacia funkcija
-  let calc1 = Calculator();
-  let calc2 = Calculator();
-  let calc3 = Calculator();
-  let calc4 = Calculator();
-  // bet tai nera tas pats objektas
-  console.log(calc1 === calc2);
-  
-  
-  console.log(calc1.sum(1, 1));
-  console.log(calc3.division(1,1));
-  console.log(calc2.subtraction(3, 3));
-  console.log(calc4.multiplication(1,1));
-  
-  
-  
+function showObjectKeys(make, model, year, color){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+}
+let showObjectKeysArray=[audi] ;
+
+//console.log(showObjectKeysArray)
