cec83c491d25d9024196afa5414b60403735e990
ernestazajancauskaite <101720004+ernestazajancauskaite@users.noreply.github.com>
2022-06-04 13:24:33 +0300

    Task 5

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-/* ------------------------------ TASK 9 ---------------------------------------------------------------
-Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri gebės sukurti objektus 3 savybėm ir 1 metodu.
+/* ------------------------------ TASK 5 -----------------------------------
+Turimas "users" masyvas. 
 
-Savybės:
-title: string
-director: string
-budget: number
+Parašykite funckijas, kurios atlikas nurodytas užduotis:
+1. funkcija "filterDogOwers" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins "users", kurie turi augintinį.
+2. funkcija "filterAdults" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins masyvą su "users", kurie yra pilnamečiai.
+-------------------------------------------------------------------------- */
 
-Metodas:
-wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false.
------------------------------------------------------------------------------------------------------- */
+const users = [
+  { id: '1', name: 'John Smith', age: 20, hasDog: true },
+  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
+  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
+  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
+  { id: '5', name: 'Alex John', age: 25, hasDog: true },
+  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
+  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
+  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
+  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+];
 
-class Movie { 
-    constructor (title, director, budget) {
-      this.title = String(title);
-      this.director = String(director);
-      this.budget = Number(budget);
-    }
-    wasExpensive = this.budget > 100000000 ? "true" : "false";
-     }
-     
-     
+const filterDogOwners = users.filter ((users)=> {
+  return users.hasDog === true;
+})
+ console.log(filterDogOwners)
+
+
+const filterAdults = users.filter((users) => {
+ return users.age > 18;
+});
+console.log(filterAdults)
+
+
